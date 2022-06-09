ba149045ae76fc3034d6fbfdece71cc434c74f3c
ShemBrooklyne <shem.mwangi@cellulant.io>
2022-06-09 22:11:55 +0300

    use of registry binding and sql operations

---
+++ src/main/java/SQLOpersations/ResultHandler.java
@@ -0,0 +1,13 @@
+package SQLOpersations;
+
+import java.util.List;
+
+public class ResultHandler {
+
+    public void printResult(List list) {
+
+        for (Object o : list) System.out.println(o);
+
+    }
+
+}
---
+++ src/main/java/SQLOpersations/SqlOperation.java
@@ -0,0 +1,44 @@
+package SQLOpersations;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.support.SimpleRegistry;
+
+import java.util.Map;
+
+/**
+ * @author shem.mwangi
+ */
+public class SqlOperation {
+
+    public static void main(String[] args) throws Exception {
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/apache_camel");
+        dataSource.setUser("root");
+        dataSource.setPassword("m3tr0crus@d3r");
+
+        CamelContext context = new DefaultCamelContext();
+
+        context.getRegistry().bind("myDataSource", dataSource);
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+
+                from("direct:start")
+                        .to("jdbc:myDataSource")
+                        .bean(new ResultHandler(), "printResult");
+
+            }
+        });
+        context.start();
+
+        ProducerTemplate producerTemplate = context.createProducerTemplate();
+        producerTemplate.sendBody("direct:start", "select * from customer");
+    }
+
+}
